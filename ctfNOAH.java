ce9c8d82550e0acc04209f4ff1ba7478aaedd0ab
Ryan Chen <ryanjchen@yahoo.com>
2017-08-01 10:27:10 -0700

    i might have it

--- ctfNOAH.java
+++ ctfNOAH.java
@@ -1,22 +1,39 @@
+import Java.util.ArrayLists;
 public class ctfNOAH{
 	int key = 1471587914;
 	int lowkey = 1472541258;
 	int res = 0;
 	public static void main(String[] args){
 		int res=0;
-		int[] bytes = new int[6];
+		ArrayList<int> list = new ArrayList<int>();
+		try{
+			rescursiveFind(key, 5);
+		}catch(StackOverFlowError e){
+			e.printStackTrace();
+		}
+
+		for(int i = 0; i < list.size(); i++){
+			System.out.println(list.get(i));
+		}
 	}
 
-	private void rescursiveFind(int key, int counter){
-		switch(counter){
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-				key - 31;
-				
+	private void rescursiveFind(int rem_val, int counter){
+		if(rem_val >= 32 && rem_val <= 126){ 
+			System.out.println(rem_val);
+		}else{
+			if(counter > 0){
+				for(int i = 32; i <= 126; i++){
+					double new_val = (rem_val - i) / 31;
+					if((rem_val % 1) > 0){ //if rem_val is decimal
+						break; //failed, then ded and break out of for loop
+					}else{
+						list.add(i);
+						rescursiveFind(rem_val, counter - 1); //keep going
+					}
+				}
+			}else{
+				list.clear();
+			}
 		}
 	}
 	
