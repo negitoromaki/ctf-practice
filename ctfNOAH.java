342bd5ef6193de5b7206c36a7164700bf5430477
Noah Curran <swimnoah113@gmail.com>
2017-08-01 12:05:40 -0500

    added formula to threads

--- ctfNOAH.java
+++ ctfNOAH.java
@@ -1,17 +1,34 @@
 public class ctfNOAH{
   
-  private static class CalculationThread extends Thread {
+  private static class CalculationThread1 extends Thread {
     public void run() {
       System.out.println("Creating thread1");
+      calculate(32,56);
     }
   }
   
-  public static void run(){
-    
-    Thread calculate;
-    // run thread
-    calculate = new Thread(new ctfNOAH.CalculationThread());
-    calculate.start();
+  private static class CalculationThread2 extends Thread {
+    public void run() {
+      System.out.println("Creating thread2");
+      calculate(57,80);
+    }
+  }
+  
+  private static class CalculationThread3 extends Thread {
+    public void run() {
+      System.out.println("Creating thread3");
+      calculate(81,104);
+    }
+  }
+  
+  private static class CalculationThread4 extends Thread {
+    public void run() {
+      System.out.println("Creating thread4");
+      calculate(105,126);
+    }
+  }
+  
+  public static void calculate(int first, int last){
     
     // The given key
     int key = 1471587914;
@@ -26,7 +43,7 @@ public class ctfNOAH{
         for (int c = 32; c <= 126; c++) {
           for (int d = 32; d <= 126; d++) {
             for (int e = 32; e <= 126; e++) {
-              for (int f = 32; f <= 126; f++) {
+              for (int f = first; f <= last; f++) {
                 
                 // Calculate the hash.
                 double test = a*Math.pow(31, 5) + b * Math.pow(31,4) + c * Math.pow(31, 3) + d * Math.pow(31, 2) + e * Math.pow(31, 1) + f;
@@ -52,6 +69,23 @@ public class ctfNOAH{
         }
       }
     }
+  }
+  
+  public static void run() {
+    Thread calculate1, calculate2, calculate3, calculate4;
+    // run thread
+    calculate1 = new Thread(new ctfNOAH.CalculationThread1());
+    calculate1.start();
+    
+    calculate2 = new Thread(new ctfNOAH.CalculationThread2());
+    calculate2.start();
+    
+    calculate3 = new Thread(new ctfNOAH.CalculationThread3());
+    calculate3.start();
+    
+    calculate4 = new Thread(new ctfNOAH.CalculationThread4());
+    calculate4.start();
+    
   }
   
   public static void main(String[] args){
